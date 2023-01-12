b14fcac98b6f486eedf3f31b864b4f570cf6be03
its22013 <s22013@std-college.ac.jp>
2023-01-12 07:37:41 +0000

    first commit

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
+import Container form 'components/container'
 import Hero from 'components/hero'
 
 export default function About() {
   return (
-     <Hero 
-       title="About"
-       subtitle="About development activitis"
-     />
+    <Container>
+     <Hero title="About" subtitle="About development activitis" />
+    </Container>
    )
 }
